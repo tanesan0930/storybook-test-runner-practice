32accfd37f9132fd189f0ef565ba9be3004b8a75
tanesan0930 <ryo.hirata@mmmcorp.co.jp>
2023-08-14 18:52:35 +0900

    update UserForm

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,9 +1,28 @@
+import { useRef } from 'react';
+
 export const UserForm = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(
+      `Name: ${nameRef.current?.value}\nAge: ${ageRef.current?.value}\nEmail: ${emailRef.current?.value}`
+    );
+  };
+
   return (
-    <form style={{ display: 'flex', flexDirection: 'column', width: '400px' }}>
-      <input placeholder="name" />
-      <input placeholder="age" />
-      <input placeholder="email" />
+    <form
+      style={{ display: 'flex', flexDirection: 'column', width: '400px' }}
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="name">Name</label>
+      <input id="name" placeholder="name" ref={nameRef} />
+      <label htmlFor="age">Age</label>
+      <input id="age" placeholder="age" type="number" ref={ageRef} />
+      <label htmlFor="email">Email</label>
+      <input id="email" placeholder="email" type="email" ref={emailRef} />
       <button type="submit">Submit</button>
     </form>
   );
